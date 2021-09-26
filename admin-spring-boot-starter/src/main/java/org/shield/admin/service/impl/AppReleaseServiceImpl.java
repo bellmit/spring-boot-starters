a1769ed2d1b7e36ee17e2abfdd3bd55047b62244
zacksleo <zacksleo@gmail.com>
2021-09-26 14:07:24 +0800

    fix(admin): fix app-release query

--- admin-spring-boot-starter/src/main/java/org/shield/admin/service/impl/AppReleaseServiceImpl.java
+++ admin-spring-boot-starter/src/main/java/org/shield/admin/service/impl/AppReleaseServiceImpl.java
@@ -22,6 +22,6 @@ public class AppReleaseServiceImpl extends AbstractService<AppRelease> implement
     @Override
     public List<AppRelease> list(AppReleaseQueryForm form) {
         Condition condition = form.toCondition();
-        return condition.createCriteria().getAllCriteria().isEmpty() ? findAll() : findByCondition(condition);
+        return condition.getOredCriteria().isEmpty() ? findAll() : findByCondition(condition);
     }
 }
