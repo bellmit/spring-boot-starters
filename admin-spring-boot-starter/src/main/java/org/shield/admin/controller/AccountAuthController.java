a31a207103918af2da194af9830e47f41546a715
zacksleo <zacksleo@gmail.com>
2021-11-04 18:02:33 +0800

    feat(admin): 某个用户，可以按认证方式查询一条记录

--- admin-spring-boot-starter/src/main/java/org/shield/admin/controller/AccountAuthController.java
+++ admin-spring-boot-starter/src/main/java/org/shield/admin/controller/AccountAuthController.java
@@ -1,6 +1,7 @@
 package org.shield.admin.controller;
 
 import java.util.List;
+import org.shield.admin.mapper.AdminAccountAuthMapper;
 import org.shield.admin.model.AdminAccountAuth;
 import org.shield.admin.service.AdminAccountAuthService;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +24,25 @@ public class AccountAuthController {
     @Autowired
     private AdminAccountAuthService service;
 
+    @Autowired
+    private AdminAccountAuthMapper mapper;
+
     @ApiOperation("查询")
     @GetMapping
-    public List<AdminAccountAuth> list(@PathVariable("adminId") String checkerId) {
+    public List<AdminAccountAuth> list(@PathVariable("adminId") String adminId) {
         Condition condition = new Condition(AdminAccountAuth.class);
         Criteria criteria = condition.createCriteria();
-        criteria.andEqualTo("accountId", checkerId);
+        criteria.andEqualTo("accountId", adminId);
         return service.findByCondition(condition);
+    }
+
+    @ApiOperation("查询单个认证方式")
+    @GetMapping("/sources/{source}")
+    public AdminAccountAuth findBySource(@PathVariable("adminId") String adminId, @PathVariable("source") String source) {
+        Condition condition = new Condition(AdminAccountAuth.class);
+        Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("accountId", adminId);
+        criteria.andEqualTo("source", source);
+        return mapper.selectOneByExample(condition);
     }
 }
