bdf78de6c0d1ba3bc0386d5b2564b06cc7e0d054
zacksleo <zacksleo@gmail.com>
2021-11-29 16:02:12 +0800

    refact(service): 请求表单默认将空字符串转为null

--- service-spring-cloud-starter/src/main/java/org/shield/service/autoconfigure/ServiceAutoConfiguration.java
+++ service-spring-cloud-starter/src/main/java/org/shield/service/autoconfigure/ServiceAutoConfiguration.java
@@ -1,17 +1,27 @@
 package org.shield.service.autoconfigure;
 
 import org.shield.service.converter.HeaderDecoderConverter;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
 
 /**
  * @author zacksleo@gmail.com
  */
 @Configuration
+@ControllerAdvice
 public class ServiceAutoConfiguration {
 
     @Bean
     public HeaderDecoderConverter headerDecoderConverter() {
         return new HeaderDecoderConverter();
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 }
